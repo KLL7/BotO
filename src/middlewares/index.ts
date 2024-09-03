8329e6ad6f4c992437b52eacac6851b31ac4fa8a
Matusalém <matusalemdedeus@gmail.com>
2024-09-02 22:42:37 -0300

    set app port and add cors comment

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,11 +1,13 @@
-import cors from "cors";
+import cors from "cors"; // Maybe add a file exporting cors with all options
 import express from "express";
 
 // TODO: Add options to cors
 
 // Call this function in config/app
 // This function receives an express app and adds all middlewares configurations to it
-export const useMiddlewares = (app: any) => {
+export const useMiddlewares = (app: express.Application) => {
+  app.set("port", process.env.PORT || 3000);
+  
   app.use(cors());
 
   app.use(express.json());
