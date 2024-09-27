4d626d2eb4a8d0cac4bdbbbc7970cc98e673ee90
Matusalém <matusalemdedeus@gmail.com>
2024-09-27 10:14:47 -0300

    feat: telegramBot random greeting working
    feat: IProfessional and implementation
    
    the project strutucture has changed. The previous one was not good for the bot structure.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,36 @@
-import app from "./config/app";
+import TelegramBot from "node-telegram-bot-api";
+import "dotenv/config";
+import Professional from "./Professional";
 
-app.listen(app.get("port"), () => {
-  console.log(`Server on port ${app.get("port")}`);
-})
+const telBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!, {
+  polling: true,
+});
+
+const testProfessional = new Professional(
+  "João Alberto",
+  "joãoalberto@test.me"
+);
+
+testProfessional.setGreetings([
+  "Olá! Sou da área de aprimoramento de espelhos quebrados. Estou aqui para te ajudar.",
+  "Oi, tudo bem? Estou aqui para poder te atender, como posso te chamar?",
+  "Oi, tudo bem? Sou o João Alberto. Estou aqui para te ajudar. Como posso te ajudar?",
+  "Essa é mais uma mensagem de teste de saudação.",
+  "Manda a braba",
+  "Murilo Gomes Bot ao seu dispor."
+])
+
+telBot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  let greeting = testProfessional.getRandomGreeting();
+ 
+  if (!greeting) {
+    greeting = `
+      Olá! Me chamo ${testProfessional.getName()}.
+      Como posso te ajudar?
+    `;
+  }
+
+  telBot.sendMessage(chatId, greeting);
+});
