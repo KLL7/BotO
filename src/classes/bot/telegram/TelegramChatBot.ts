aca62082200c95efdd0cd314da023e6438471292
Matusalém de Sousa <124395470+IsaacSales15@users.noreply.github.com>
2024-10-21 11:01:12 -0300

    Corpus add

--- src/classes/bot/telegram/TelegramChatBot.ts
+++ src/classes/bot/telegram/TelegramChatBot.ts
@@ -21,6 +21,8 @@ export default class TelegramChatBot extends ChatBot {
       "fazer um agendamento",
       "queria agendar",
       "agendar consulta",
+      "quero agendar",
+      
     ]);
   }
 
